0316b5c67c48da4fc73cb077fee8459799ee0e00
Jan Giacomelli <jan@typless.com>
2020-10-28 15:06:37 +0100

    Initial commit

---
+++ test_app.py
@@ -0,0 +1,17 @@
+import pytest
+
+from app import app
+
+
+@pytest.fixture
+def client():
+    app.config["TESTING"] = True
+
+    with app.test_client() as client:
+        yield client
+
+
+def test_home(client):
+    response = client.get("/")
+
+    assert response.status_code == 200
